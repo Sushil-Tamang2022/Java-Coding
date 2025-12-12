dc2bf6dda4bdf729ef719a2dce37c3b4eada10af
Sushil-Tamang2022 <sushiltamang411@gmail.com>
2025-12-12 22:24:44 +0545

    initial commit

---
+++ FirstProgram.java
@@ -0,0 +1,17 @@
+public class FirstProgram {
+    public static void main(String[] args) {
+
+        // This is my first Java Program
+        /*
+         * This
+         * is
+         * a
+         * multi-line
+         * comment
+         */
+        System.out.println("I like pizza!");
+        System.out.println("Its really good!");
+        System.out.println("Buy me pizza!");
+    }
+}
+
